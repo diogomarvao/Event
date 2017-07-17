771351e52e90d666410e4afb3bbc7c8cc03cf9c4
FredericoMonteiro <frederico.dmonteiro@gmail.com>
2017-07-17 19:50:18 +0100

    dfg

--- src/main/java/com/musicstadium/model/Event.java
+++ src/main/java/com/musicstadium/model/Event.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Future;
 
